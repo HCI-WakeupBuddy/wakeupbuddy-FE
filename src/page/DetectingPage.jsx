c9fa67dad52467ce26e7126312a6b247827b239b
ynzung <syj91105@sookmyung.ac.kr>
2024-11-28 19:48:44 +0900

    Feat: 장치착용

--- src/page/DetectingPage.jsx
+++ src/page/DetectingPage.jsx
@@ -1,20 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
+import { useNavigate } from "react-router-dom";
 import { BeatLoader } from "react-spinners";
 import TitleText from "../components/common/TitleText";
+import Lottie from "lottie-react";
+import Check from "../animations/checkmark.json";
 
 const DetectingPage = () => {
+  const [state, setState] = useState(false);
   const username = localStorage.getItem("username");
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setState(true);
+      setTimeout(() => {
+        navigate("/setting");
+      }, 3000);
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [navigate]);
+
   return (
     <div>
       <Wrapper>
         <TitleText txt={"측정 중"} />
-        <SubText>
-          <span>{username}님</span>의 뇌파를 측정 중입니다.
-        </SubText>
-        <LoadingBox>
-          <BeatLoader color="#6750A4" />
-        </LoadingBox>
+        {state ? (
+          <div>
+            <SubText>
+              <span>{username}님,</span>
+              <br />
+              WakeupBuddy가 올바르게 착용되었습니다 :)
+            </SubText>
+            <CheckBox>
+              <Lottie
+                animationData={Check}
+                loop={false}
+                autoplay={true}
+                style={{ width: 65, height: 65 }}
+              />
+            </CheckBox>
+          </div>
+        ) : (
+          <div>
+            <SubText>
+              <span>{username}님,</span>
+              <br />
+              WakeupBuddy를 착용해주세요.
+            </SubText>
+            <LoadingBox>
+              <BeatLoader color="#6750A4" />
+            </LoadingBox>
+          </div>
+        )}
       </Wrapper>
     </div>
   );
@@ -32,6 +72,7 @@ const Wrapper = styled.div`
 `;
 
 const SubText = styled.div`
+  text-align: center;
   margin-top: 138px;
   color: #000;
   font-family: "Pretendard";
@@ -40,8 +81,18 @@ const SubText = styled.div`
   font-weight: 400;
   line-height: 150%;
   > span {
-    font-weight: 700;
+    font-size: 18px;
+    font-weight: 600;
   }
 `;
+const CheckBox = styled.div`
+  margin-top: 55px;
+  display: flex;
+  justify-content: center;
+`;
 
-const LoadingBox = styled.div``;
+const LoadingBox = styled.div`
+  margin-top: 53px;
+  display: flex;
+  justify-content: center;
+`;
