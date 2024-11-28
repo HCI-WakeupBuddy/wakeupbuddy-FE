bc7899315da0a003c9f5200f9256a91c37792c63
ynzung <syj91105@sookmyung.ac.kr>
2024-11-28 15:54:38 +0900

    Feat: 학습리포트 작성 페이지 텍스트 생성

--- src/page/WritingReport.jsx
+++ src/page/WritingReport.jsx
@@ -3,10 +3,16 @@ import styled from "styled-components";
 import TitleText from "../components/common/TitleText";
 
 const WritingReport = () => {
+  const username = localStorage.getItem("username");
   return (
     <div>
       <Wrapper>
         <TitleText txt={"WakeupBuddy"} />
+        <SubText>
+          <span>{username}님</span>의 뇌파 데이터를 기반으로
+          <br />
+          <span style={{ color: "#6750A4" }}>학습 리포트</span>를 작성 중입니다
+        </SubText>
       </Wrapper>
     </div>
   );
@@ -20,4 +26,17 @@ const Wrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
+`;
+
+const SubText = styled.div`
+  margin-top: 180px;
+  text-align: center;
+  color: #000;
+  font-family: "pretendard";
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 150%;
+  > span {
+    font-weight: 700;
+  }
 `;
